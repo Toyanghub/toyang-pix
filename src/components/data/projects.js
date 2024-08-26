05d1309d57ef52b284e4203fefb1588846bb952a
Selwyn Tayong <selwyntayong12@gmail.com>
2024-08-27 07:59:52 +0800

    Add/update resume, project, skills, and work data

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,22 +1,32 @@
 export const ProjectData = [
   {
     id: 1,
-    title: 'Ca Faith Clothing Line',
-    about: 'Good quality clothes can be found on this website',
-    tags: ['HTML', 'CSS', 'Bootstrap', 'Javascript'],
-    demo: 'https://clothing-line-toyangdev.netlify.app/',
+    title: "Ca Faith Clothing Line",
+    about: "Good quality clothes can be found on this website",
+    tags: ["HTML", "CSS", "Bootstrap", "Javascript"],
+    demo: "https://clothing-line-toyangdev.netlify.app/",
     // github: 'COMING SOON',
     image:
-      'https://clothing-line-toyangdev.netlify.app/assets/img/portfolio/12.jpg',
+      "https://clothing-line-toyangdev.netlify.app/assets/img/portfolio/12.jpg",
   },
 
   {
     id: 2,
-    title: 'Portfolio V1',
-    about: 'Showing my few projects, I did while I was in college',
-    tags: ['HTML', 'CSS', 'Bootstrap', 'Javascript'],
-    demo: 'https://toyangdev.netlify.app/',
-    github: 'https://github.com/Toyanghub/Resume',
-    image: 'https://toyangdev.netlify.app/assets/img/img-3.jpg',
+    title: "Portfolio V1",
+    about: "Showing my few projects, I did while I was in college",
+    tags: ["HTML", "CSS", "Bootstrap", "Javascript"],
+    demo: "https://toyangdev.netlify.app/",
+    github: "https://github.com/Toyanghub/Resume",
+    image: "https://toyangdev.netlify.app/assets/img/img-3.jpg",
+  },
+  {
+    id: 3,
+    title: "Milktea Shop",
+    about:
+      "Indulge in our meticulously crafted milk tea flavors, designed to deliver a delightful and refreshing experience. Discover our signature drinks and treat yourself to a new favorite today!",
+    tags: ["HTML", "CSS", "Javascript"],
+    demo: "https://milkytea.netlify.app/",
+    github: "https://github.com/Toyanghub/MilkTea",
+    image: "https://milkytea.netlify.app/img/sample_milk_4.jpg",
   },
 ];
--- src/components/data/skills.js
+++ src/components/data/skills.js
@@ -7,20 +7,20 @@ export const SkillData = [
         icon: <i class="devicon-react-original colored"></i>,
       },
       {
+        name: "VueJS",
+        icon: <i class="devicon-vuejs-plain colored"></i>,
+      },
+      {
         name: "Javascript",
         icon: <i class="devicon-javascript-plain colored"></i>,
       },
       {
-        name: "Figma",
-        icon: <i class="devicon-figma-plain colored"></i>,
+        name: "HTML",
+        icon: <i class="devicon-html5-plain-wordmark colored"></i>,
       },
       {
-        name: "Adobe Illustrator",
-        icon: <i class="devicon-illustrator-plain colored"></i>,
-      },
-      {
-        name: "Photoshop",
-        icon: <i class="devicon-photoshop-plain colored"></i>,
+        name: "CSS",
+        icon: <i class="devicon-css3-plain-wordmark colored"></i>,
       },
     ],
   },
@@ -38,6 +38,35 @@ export const SkillData = [
       {
         name: "MySQL",
         icon: <i class="devicon-mysql-plain-wordmark colored"></i>,
+      },
+      {
+        name: "SSMS",
+        icon: <i class="devicon-microsoftsqlserver-plain-wordmark"></i>,
+      },
+    ],
+  },
+  {
+    type: "Tools",
+    list: [
+      {
+        name: "Git",
+        icon: <i class="devicon-git-plain colored"></i>,
+      },
+      {
+        name: "Github",
+        icon: <i class="devicon-github-original colored"></i>,
+      },
+      {
+        name: "Figma",
+        icon: <i class="devicon-figma-plain colored"></i>,
+      },
+      {
+        name: "Adobe Illustrator",
+        icon: <i class="devicon-illustrator-plain colored"></i>,
+      },
+      {
+        name: "Photoshop",
+        icon: <i class="devicon-photoshop-plain colored"></i>,
       },
     ],
   },
