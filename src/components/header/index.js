7ef67b0697a85b0b2524463ebbdcf4d50d32df56
Selwyn Tayong <selwyntayong12@gmail.com>
2025-03-03 02:17:28 +0800

    Fix header for responsiveness and for pc and mobile view as well, fix hamburger menu for better user exp

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./header.css";
 import Mobile from "./mobile";
 import Web from "./web";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  }, [isOpen]);
+
   return (
     <div className="header">
       <div className="logo">Toyang</div>
@@ -14,7 +24,7 @@ function Header() {
         </div>
         <div className="mobile-menu">
           <div onClick={() => setIsOpen(!isOpen)}>
-            <i class="fi fi-bs-bolt menu-icon"></i>
+            <i className="fi fi-bs-bolt menu-icon"></i>
           </div>
           {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen} />}
         </div>
--- src/components/header/mobile/index.js
+++ src/components/header/mobile/index.js
@@ -1,34 +1,32 @@
-import React from 'react';
-import './mobile.css';
+import React from "react";
+import "./mobile.css";
 
 function Mobile({ isOpen, setIsOpen }) {
   return (
-    <div className="mobile">
+    <div className={`mobile-menu ${isOpen ? "open" : ""}`}>
       <div className="close-icon" onClick={() => setIsOpen(!isOpen)}>
-        <i class="fi fi-ss-bolt"></i>
+        <i className="fi fi-ss-bolt"></i>
       </div>
       <div className="mobile-options">
-        <div className="mobile">
-          <div className="mobile-option">
-            <a href="#projects">
-              <i class="fi fi-bs-rocket-lunch option-icon"></i>Projects
-            </a>
-          </div>
-          <div className="mobile-option">
-            <a href="#skills">
-              <i class="fi fi-bs-gym option-icon"></i>Skills
-            </a>
-          </div>
-          <div className="mobile-option">
-            <a href="#work">
-              <i class="fi fi-bs-hourglass-end option-icon"></i>Work
-            </a>
-          </div>
-          <div className="mobile-option">
-            <a href="#contact">
-              <i class="fi fi-bs-world option-icon"></i>Contact
-            </a>
-          </div>
+        <div className="mobile-option">
+          <a href="#projects" onClick={() => setIsOpen(false)}>
+            <i className="fi fi-bs-rocket-lunch option-icon"></i>Projects
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="#skills" onClick={() => setIsOpen(false)}>
+            <i className="fi fi-bs-gym option-icon"></i>Skills
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="#work" onClick={() => setIsOpen(false)}>
+            <i className="fi fi-bs-hourglass-end option-icon"></i>Work
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="#contact" onClick={() => setIsOpen(false)}>
+            <i className="fi fi-bs-world option-icon"></i>Contact
+          </a>
         </div>
       </div>
     </div>
