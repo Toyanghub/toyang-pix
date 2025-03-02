cfe2d41d72185fcff7ec0a32604541b07a5fe11b
Selwyn Tayong <selwyntayong12@gmail.com>
2025-03-02 16:36:21 +0800

    Add new project and improve responsiveness

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import SocialContact from '../../common/social-contact';
-import './about.css';
+import React from "react";
+import SocialContact from "../../common/social-contact";
+import "./about.css";
+
+// function About() {
+//   return (
+//     <div className="about">
+//       <div className="about-top">
+//         <div className="about-info">
+//           Hey there! 👋 I'm <br />{" "}
+//           <span className="info-name">Selwyn Tayong.</span>
+//           <br /> Crafting code with a smile. Let's build something awesome!
+//         </div>
+//         <div className="about-photo">
+//           <img
+//             src={require("../../../assets/coding.png")}
+//             className="picture"
+//           />
+//         </div>
+//       </div>
+//       <SocialContact />
+//     </div>
+//   );
+// }
+
 function About() {
   return (
     <div className="about">
       <div className="about-top">
         <div className="about-info">
-          Hello 👋, my name is <br />{' '}
-          <span className="info-name">Selwyn Tayong.</span>
-          <br /> I'm a front/back end developer.
+          <h1>
+            Hey there! 👋 I'm <br />
+            <span className="info-name">Selwyn Tayong.</span>
+          </h1>
+          <p>Crafting code with a smile. Let's build something awesome!</p>
         </div>
         <div className="about-photo">
           <img
-            src={require('../../../assets/coding.png')}
+            src={require("../../../assets/coding.png")}
             className="picture"
+            alt="Developer illustration"
           />
         </div>
       </div>
-      <SocialContact />
+      <div className="about-social">
+        <SocialContact />
+      </div>
     </div>
   );
 }
--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,32 +1,43 @@
 export const ProjectData = [
-  {
-    id: 1,
-    title: "Ca Faith Clothing Line",
-    about: "Good quality clothes can be found on this website",
-    tags: ["HTML", "CSS", "Bootstrap", "Javascript"],
-    demo: "https://clothing-line-toyangdev.netlify.app/",
-    // github: 'COMING SOON',
-    image:
-      "https://clothing-line-toyangdev.netlify.app/assets/img/portfolio/12.jpg",
-  },
+  // {
+  //   id: 1,
+  //   title: "Ca Faith Clothing Line",
+  //   about: "Good quality clothes can be found on this website",
+  //   tags: ["HTML", "CSS", "Bootstrap", "Javascript"],
+  //   demo: "https://clothing-line-toyangdev.netlify.app/",
+  //   // github: 'COMING SOON',
+  //   image:
+  //     "https://clothing-line-toyangdev.netlify.app/assets/img/portfolio/12.jpg",
+  // },
+
+  // {
+  //   id: 2,
+  //   title: "Portfolio V1",
+  //   about: "Showing my few projects, I did while I was in college",
+  //   tags: ["HTML", "CSS", "Bootstrap", "Javascript"],
+  //   demo: "https://toyangdev.netlify.app/",
+  //   github: "https://github.com/Toyanghub/Resume",
+  //   image: "https://toyangdev.netlify.app/assets/img/img-3.jpg",
+  // },
+  // {
+  //   id: 3,
+  //   title: "Milktea Shop",
+  //   about:
+  //     "Indulge in our meticulously crafted milk tea flavors, designed to deliver a delightful and refreshing experience. Discover our signature drinks and treat yourself to a new favorite today!",
+  //   tags: ["HTML", "CSS", "Javascript"],
+  //   demo: "https://milkytea.netlify.app/",
+  //   github: "https://github.com/Toyanghub/MilkTea",
+  //   image: "https://milkytea.netlify.app/img/sample_milk_4.jpg",
+  // },
 
   {
-    id: 2,
-    title: "Portfolio V1",
-    about: "Showing my few projects, I did while I was in college",
-    tags: ["HTML", "CSS", "Bootstrap", "Javascript"],
-    demo: "https://toyangdev.netlify.app/",
-    github: "https://github.com/Toyanghub/Resume",
-    image: "https://toyangdev.netlify.app/assets/img/img-3.jpg",
-  },
-  {
-    id: 3,
-    title: "Milktea Shop",
+    id: 1,
+    title: "My Dev Diaries",
     about:
-      "Indulge in our meticulously crafted milk tea flavors, designed to deliver a delightful and refreshing experience. Discover our signature drinks and treat yourself to a new favorite today!",
-    tags: ["HTML", "CSS", "Javascript"],
-    demo: "https://milkytea.netlify.app/",
-    github: "https://github.com/Toyanghub/MilkTea",
-    image: "https://milkytea.netlify.app/img/sample_milk_4.jpg",
+      "Welcome to my tech haven! Dive into a mix of tech tips and personal musings. Whether you're here to learn or just chill, enjoy the ride! 🚀",
+    tags: ["Next.js", "Contentlayer", "Tailwind CSS", "Dark Mode"],
+    demo: "https://toyang-blog.vercel.app/",
+    github: "https://github.com/Toyanghub/nextjs-blog",
+    image: require("../../assets/blog-image.jpg"),
   },
 ];
