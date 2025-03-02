9dbac045f8000bc230c2c8e8d1d1ced8f2cde449
Selwyn Tayong <selwyntayong12@gmail.com>
2025-03-02 23:23:07 +0800

    Fix here in Footer the UI and responsiveness for user to easier navigate

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 import "./footer.css";
+
+// function Footer() {
+//   return (
+//     <div className="footer">
+//       <div className="footer-content">
+//         Created and Designed <span className="emoji">🔥</span> by{" "}
+//         <span className="author-name">Selwyn Tayong</span>{" "}
+//         <span className="year">© {new Date().getFullYear()}</span>
+//       </div>
+//     </div>
+//   );
+// }
+
 function Footer() {
   return (
     <div className="footer">
-      Created and Designed 🔥 by Selwyn Tayong © 2024
+      <div className="footer-content">
+        Crafted with passion <span className="emoji">🔥</span> by{" "}
+        <span className="author-name">Selwyn Tayong</span> ©{" "}
+        <span className="year">{new Date().getFullYear()}</span>
+      </div>
     </div>
   );
 }
